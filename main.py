08d13978800209adb4f03b6a56acbb475c51665c
Melike <165141178+melikecolak967@users.noreply.github.com>
2025-01-01 20:32:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def calculate(a, b, operation):
     if operation == "add":
-        return a - b
+        return a + b
     elif operation == "subtract":
-        return a - b
+        return a * b
     elif operation == "multiply":
         return a * b
     elif operation == "divide":
